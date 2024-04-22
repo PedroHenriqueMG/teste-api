8f0659ae016daa613cd4acc94cd114a1ae2da282
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-21 22:38:45 -0300

    feat: call prisma on index file

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,20 @@
 import Express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = Express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+  })
+  .catch((error: Error) => {
+    throw new Error(error.message);
+  });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
