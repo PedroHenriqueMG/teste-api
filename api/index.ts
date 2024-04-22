d355312594cc0b2e8c5dd52148662f141962867f
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-21 22:53:02 -0300

    fix: call async prisma

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,19 @@
 import Express from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../client/client";
 
 const app = Express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-prisma
-  .$connect()
-  .then(() => {
-    console.log("Conexão com o banco de dados estabelecida com sucesso.");
-  })
-  .catch((error: Error) => {
-    throw new Error(error.message);
-  });
+app.get("/", async (req, res) => {
+  await prisma
+    .$connect()
+    .then(() => {
+      console.log("Conexão com o banco de dados estabelecida com sucesso.");
+    })
+    .catch((error: Error) => {
+      throw new Error(error.message);
+    });
+  res.send("Express on Vercel");
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
