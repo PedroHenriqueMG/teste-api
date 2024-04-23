b375bbcfaebae3ba06efa29c7264503e7a63d8b1
LucasTravessa <lucas.cos.travessa@gmail.com>
2024-04-22 21:05:53 -0300

    o pae ta rato d+

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import prisma from "./client/client";
 import swaggerui from "swagger-ui-express";
 import { document } from "./swagger/swagger";
 import * as yaml from "yaml";
-import * as fs from "fs"
+import * as fs from "fs";
 
 const app = Express();
 app.use(Express.json());
@@ -22,10 +22,21 @@ prisma
     throw new Error(error.message);
   });
 
-  const file  = fs.readFileSync('./src/swagger/openapi.yml', 'utf8')
+const file = fs.readFileSync("./src/swagger/openapi.yml", "utf8");
 const teste = yaml.parse(file);
 
-app.use("/docs", swaggerui.serve, swaggerui.setup(teste));
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+
+app.use(
+  "/docs",
+  swaggerui.serve,
+  swaggerui.setup(teste, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
+);
 
 app.use(errorMiddleware);
 app.listen(process.env.PORT, () =>
